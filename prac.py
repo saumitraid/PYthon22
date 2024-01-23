c8d455fac5f2d2ca3effe46634b36b667fb2d7fe
saumitraid <saumitraid.das@gmail.com>
2024-01-23 16:22:52 +0530

    Create prac.py

---
+++ prac.py
@@ -0,0 +1,13 @@
+n1=int(input("Enter 1st number:-"))
+n2=int(input("Enter 2nd number:-"))
+sum=n1+n2
+print("The sum is",sum)
+
+
+# int()
+# float()
+# str()
+# set()
+# list()
+# tuple()
+# dict()
